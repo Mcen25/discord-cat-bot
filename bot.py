8de1fb9e45a4b125178e5f49622b7b7711c6e1c0
Matthew En <86386184+Mcen25@users.noreply.github.com>
2023-11-03 00:59:57 -0400

    RANDOM CHANGES

--- bot.py
+++ bot.py
@@ -1,20 +1,22 @@
-from discord.ext import commands
+# This example requires the 'message_content' intent.
+
 import discord
 
-description = '''A discord bot that posts a picture of cat once a day'''
 intents = discord.Intents.default()
-intents.members = True
 intents.message_content = True
 
-BOT_TOKEN = "MTE2MjU2NTUxODQ5NTA1NjAxMg.GpqnAt.9ZSF2sp_MuMF4YkFA44Mm_VwigrOy3kwlPwTqw"
-CHANNEL_ID = 1162833235219320952
+client = discord.Client(intents=intents)
 
-bot = commands.Bot(command_prefix='?', description=description, intents=intents)
-
-@bot.event
+@client.event
 async def on_ready():
-    print("Hello! Study bot is ready!")
-    channel = bot.get_channel(CHANNEL_ID)
-    await channel.send("Hello! Study bot is ready!")
+    print(f'We have logged in as {client.user}')
 
-bot.run(BOT_TOKEN)
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run('MTE2OTQ5NzcwNTc5MDMxNjU1NA.G0IadR.wLR5pXqy6PORbB-Ww1qZE00Ht0Wnl1dstKQul8')
